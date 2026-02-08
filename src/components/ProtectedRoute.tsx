b6ef6e49309a2648f95b9fba5128a975a7c79561
Pavel0504 <zudinpavel99@gmail.com>
2026-02-08 20:28:20 +0300

    35

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -33,17 +33,6 @@ export default function ProtectedRoute({ children }: ProtectedRouteProps) {
     setCheckingAccess(false);
   };
 
-  if (loading || checkingAccess) {
-    return (
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="text-center">
-          <Loader2 className="h-12 w-12 animate-spin text-orange-500 mx-auto mb-4" />
-          <p className="text-gray-600">Загрузка...</p>
-        </div>
-      </div>
-    );
-  }
-
   if (!user) {
     return <Navigate to="/login" replace />;
   }
